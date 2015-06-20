c0b7d6c0a4e8f33aa261a69f5d0dd25562e69464
alfort74 <alfort625@gmail.com>
2015-06-21 00:52:08 +0900

    Add Sass files and do refactoring on html file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,15 +8,15 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
     it "should have the base title" do
-    	visit "/static_pages/home"
+    	visit root_path
     	expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
     it "should not have a custom page title" do
-      visit "/static_pages/home"
+      visit root_path
       expect(page).not_to have_title("| Home")
     end
     
@@ -25,11 +25,11 @@ describe "Static pages" do
   describe "Help page" do
 
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
 	end
     it "should have the right title" do
-    	visit "/static_pages/help"
+    	visit help_path
     	expect(page).to have_title("#{base_title} | Help")
     end
 	
@@ -38,11 +38,11 @@ describe "Static pages" do
   describe "About page" do
 
   	it "should have the content 'About'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About Us')
   	end
     it "should have the right title" do
-    	visit "/static_pages/about"
+    	visit about_path
     	expect(page).to have_title("#{base_title} | About")
     end
   end
@@ -50,12 +50,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit "/static_pages/contact"
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     
     it "should have the right title" do
-      visit "/static_pages/contact"
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
